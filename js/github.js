e0ec790447f1a2c8f629e0561e2f76e6f0e4badf
LosmanDev <losman78@gmail.com>
2022-07-07 14:18:06 -0400

    new

---
+++ js/github.js
@@ -0,0 +1,45 @@
+class Github {
+  constructor() {
+    this.client_id = "f4138df46a8eef88d4ee";
+    this.client_secret = "f050b01b5de2d6ac9e80773b337327dfe93dbabc";
+    this.repos_count = 5;
+    this.repose_sort = "created: asc";
+  }
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
+    };
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repose_sort}client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
